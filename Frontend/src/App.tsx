2cabc0fb4c8a87f62922055002baf06d76d0b2f2
Rohit Singh <singhrohit004008@gmail.com>
2024-03-12 17:18:50 +0530

    first commit

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import { Signin } from "./Pages/Signin"
+import { Blog } from "./Pages/Blog"
+import { Signup } from "./Pages/Signup"
+import { Blogs } from "./Pages/Blogs"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { Publish } from "./Pages/Publish"
+
+
+
+function App() {
+
+  return (
+    <>
+    <BrowserRouter>
+    <Routes>
+    <Route path="/signup" element={<Signup/>}/>
+    <Route path="/signin" element={<Signin/>}/>
+    <Route path="/blog/:id" element={<Blog/>}/>
+    <Route path="/blogs" element={<Blogs/>}/>
+    <Route path="/publish" element={<Publish/>}/>
+    </Routes>
+    </BrowserRouter>
+    </>
+  )
+}
+
+export default App
---
+++ backend/prisma/migrations/20240305074753_init_schema/migration.sql
@@ -0,0 +1,23 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Blog" (
+    "id" SERIAL NOT NULL,
+    "authorId" INTEGER NOT NULL,
+    "Content" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "Published" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "Blog_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Blog" ADD CONSTRAINT "Blog_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
